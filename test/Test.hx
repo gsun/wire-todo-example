44effa0b3e7db4eb9abf144890d1e78d8e3d4a82
Sun <gsun@coriant.com>
2020-10-30 12:16:23 +0800

    add test case

--- test/Test.hx
+++ test/Test.hx
@@ -2,15 +2,23 @@ import utest.Runner;
 import utest.ui.Report;
 import utest.Assert;
 
-
 class Test {
-  public static function main() {
-    utest.UTest.run([new SimpleTest()]);
-  }
+	public static function main() {
+		utest.UTest.run([new SimpleTest()]);
+	}
 }
 
 class SimpleTest extends utest.Test {
-    public function testVoid() {
-        var todo = new TodoModel();
-    }
+	public function testVoid() {
+		var todo = new TodoModel();
+		todo.create("task1text", "task1note", false);
+
+		var count:Int = cast Wire.data(DataKeys.COUNT).value;
+		Assert.equals(count, 1);
+		var todoList:Array<String> = cast Wire.data(DataKeys.LIST).value;
+		var todoVO:TodoVO = cast Wire.data(todoList[0]).value;
+		Assert.equals(todoVO.text, "task1text");
+		Assert.equals(todoVO.note, "task1note");
+		Assert.equals(todoVO.completed, false);
+	}
 }
